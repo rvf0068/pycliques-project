c07da3b9d18d91a6f7032ebabb2149b1c94d9c66
Rafael Villarroel <rvf0068@gmail.com>
2026-03-19 18:33:20 -0600

    chore: Remove unused main script and test configuration files; update Python version requirement

--- packages/pyg6data/src/pyg6data/__init__.py
+++ packages/pyg6data/src/pyg6data/__init__.py
@@ -1,2 +1,12 @@
-def hello() -> str:
-    return "Hello from pyg6data!"
+"""Public package exports for pyg6data."""
+
+__version__ = "0.1.0"
+
+from .lists import graph_generator, list_graphs, small_torsion_graphs
+
+__all__ = [
+    "__version__",
+    "graph_generator",
+    "list_graphs",
+    "small_torsion_graphs",
+]
